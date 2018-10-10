14a81617104f91d05bcf59a0a4629fe711449079
Arna Jónasardóttir <arnaj17@ru.is>
2018-10-10 22:49:22 +0000

    string with one or two numbers returns the sum

--- StringCalc.test.js
+++ StringCalc.test.js
@@ -1,3 +1,13 @@
+const Add = require('tdd');
+
 test('empty string returns 0', () => {
-  expect("").toBe(0);
+  expect(Add("")).toBe(0);
+});
+
+test('string with one number returns the number', () => {
+  expect(Add("1")).toBe(1);
+});
+
+test('string with two number returns the sum', () => {
+  expect(Add("12,20")).toBe(32);
 });
--- tdd.js
+++ tdd.js
@@ -2,6 +2,16 @@ function Add(numbers) {
 	if(numbers == "") {
 		return 0;
 	}
+
+	else {
+		var sum = 0;
+		var splitNums = numbers.split(",", 2);
+
+		for(var i = 0; i < splitNums.length; i++) {
+			sum += parseInt(splitNums[i]);
+		}
+		return sum;
+	}
 }
 
 module.exports = Add;
